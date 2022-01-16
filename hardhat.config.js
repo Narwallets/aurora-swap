9df9badb326474f08fe9384b665af616d03849bd
batdefi <lea0923@gmail.com>
2022-01-16 17:12:52 -0300

    aurora deployment config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const fs = require('fs');
+const privateKey = fs.readFileSync(".secret").toString().trim();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
+  networks: {
+    aurora: {
+      url: "https://mainnet.aurora.dev",
+      accounts: [privateKey]
+    },
+    aurora_testnet: {
+      url: "https://testnet.aurora.dev",
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.4",
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const { ethers, network } = require("hardhat");
+const deploySettings = require("../deploySettings.json");
+
+function toETH(value) {
+  value = value.toString();
+  return ethers.utils.parseEther(value);
+}
+
+async function main() {
+  let { wNearPrice, stNearPrice } = deploySettings[network.name]
+
+  const AuroraStNear = await ethers.getContractFactory("AuroraStNear");
+  const auroraStNear = await AuroraStNear.deploy(toETH(wNearPrice), toETH(stNearPrice));
+
+  await auroraStNear.deployed();
+
+  console.log("AuroraStNear contract: ", auroraStNear.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
